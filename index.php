f8386ee15052dd32faf7f7d5bd271d6c23789938
ignacia reyes <ignacia.reyes2@mail.udp.cl>
2026-05-14 07:47:34 -0400

    prueba de rama

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <!--begin::Head-->
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Programacion web</title>
+    <title>Programacion web php con adminLTE</title>
 
     <!--begin::Accessibility Meta Tags-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
